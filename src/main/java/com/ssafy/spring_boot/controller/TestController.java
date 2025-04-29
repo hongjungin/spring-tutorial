899e9b953430bb1871fba21977bb51c020ade539
hongjungin <jungini0000@gmail.com>
2025-04-30 00:00:52 +0900

    1주차 미션

---
+++ src/main/java/com/ssafy/spring_boot/controller/TestController.java
@@ -0,0 +1,4 @@
+package com.ssafy.spring_boot.controller;
+
+public class TestController {
+}
---
+++ src/main/java/com/ssafy/spring_boot/service/TestService.java
@@ -0,0 +1,20 @@
+package com.ssafy.spring_boot.service;
+
+import com.ssafy.spring_boot.domain.Test;
+import com.ssafy.spring_boot.repository.TestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+    private final TestRepository testRepository;
+
+    @Transactional
+    public List<Test> findAllTests() {
+        return testRepository.findAll();
+    }
+}
