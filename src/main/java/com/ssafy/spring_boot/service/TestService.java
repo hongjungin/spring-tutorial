f7fd478ea175f8206bc8197f465c461fbd2dcd4b
hongjungin <jungini0000@gmail.com>
2025-04-30 00:03:53 +0900

    1주차 미션

--- src/main/java/com/ssafy/spring_boot/service/TestService.java
+++ src/main/java/com/ssafy/spring_boot/service/TestService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class TestService {
-    private final TestRepository testRepository;
+    private TestRepository testRepository;
 
     @Transactional
     public List<Test> findAllTests() {
